04d47df7307be9d07638de58fe0c1b831efe16d4
Winston <coronell@gmail.com>
2016-10-02 11:01:00 -0500

    Part 1 completed!

---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: ` <h1>{{title}}</h1>
+				<h2>{{hero.name}} deatils!</h2>
+				<div><label>id:</label>{{hero.id}}</div>
+				<div>
+					<label>Name:</label>
+					<input [(ngModel)]="hero.name" placeholder="name">
+				</div>
+				`
+})
+
+export class AppComponent {
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}
+}
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
