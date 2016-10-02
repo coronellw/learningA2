903d180f0970bafea1624f2846f1aaee9dc302e8
Winston <coronell@gmail.com>
2016-10-02 11:03:30 -0500

    part 1 rebuild

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'my-app',
 	template: ` <h1>{{title}}</h1>
+				<h2>My Heroes</h2>
+				<ul class="heroes">
+				  <li>
+				    <!-- each hero goes here -->
+				  </li>
+				</ul>
+
 				<h2>{{hero.name}} deatils!</h2>
 				<div><label>id:</label>{{hero.id}}</div>
 				<div>
@@ -11,6 +18,7 @@ import { Component } from '@angular/core';
 				</div>
 				`
 })
+
 
 export class AppComponent {
 	hero: Hero = {
