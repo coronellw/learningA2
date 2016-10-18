271aa9f475fdf67382f1a4073d6977912d9ea0a2
Winston <coronell@gmail.com>
2016-10-17 23:25:42 -0500

    Added code to simulate 5 seconds slowness

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -9,4 +9,8 @@ export class HeroService{
 	getHeroes(): Promise<Hero[]> { 
 		return Promise.resolve(HEROES);
 	}
+
+	getHeroesSlowly():Promise<Hero[]>{
+		return new Promise<Hero[]>(resolve=>setTimeout(resolve,5000)).then(()=>this.getHeroes());
+	}
 }
