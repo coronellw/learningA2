3dee4e2cbf27a3b483a48e211bc427a0d07712a6
Wiston Coronell <corwisto@corp.suth.com>
2016-10-25 17:57:14 -0500

    Implementing routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-	imports:		[ 
+	imports: [ 
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path:'',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			},
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			}
+		])
 	],
-	declarations:	[ 
+	declarations: [ 
 		AppComponent,
-		HeroDetailComponent
+		HeroesComponent,
+		HeroDetailComponent,
+		DashboardComponent
 	],
-	bootstrap:		[ AppComponent ]
+	bootstrap: [ AppComponent ],
+	providers: [ HeroService ]
 })
 
 export class AppModule { }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,7 +4,6 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
-
 export class HeroService{ 
 	getHeroes(): Promise<Hero[]> { 
 		return Promise.resolve(HEROES);
